18ebfed59f84d82447996411e1a8855d28b1e154
PeterCypers <peter-cypers@hotmail.com>
2026-03-12 11:32:52 +0100

    video #11 9:30min

---
+++ app/Filament/Resources/CommentResource.php
@@ -0,0 +1,84 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\CommentResource\Pages;
+use App\Filament\Resources\CommentResource\RelationManagers;
+use App\Models\Comment;
+use App\Models\Post;
+use App\Models\User;
+use Filament\Forms;
+use Filament\Forms\Components\MorphToSelect;
+use Filament\Forms\Components\Select;
+use Filament\Forms\Components\TextInput;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Columns\TextColumn;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+
+class CommentResource extends Resource
+{
+    protected static ?string $model = Comment::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Select::make('user_id')->relationship('user', 'name')->preload()->searchable(),
+                TextInput::make('comment'),
+                MorphToSelect::make('commentable')
+                ->label('Commentable Poly-Morphic')
+                ->types([
+                    MorphToSelect\Type::make(Post::class)->titleAttribute('title'),
+                    MorphToSelect\Type::make(User::class)->titleAttribute('email'),
+                    MorphToSelect\Type::make(Comment::class)->titleAttribute('id'),
+                ])
+                ->searchable()
+                ->preload()
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                // more precise interactions out of scope for video #11
+                TextColumn::make('User.name'),
+                TextColumn::make('commentable_type'),
+                TextColumn::make('commentable_id'),
+                TextColumn::make('comment'),
+            ])
+            ->filters([
+                //
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListComments::route('/'),
+            'create' => Pages\CreateComment::route('/create'),
+            'edit' => Pages\EditComment::route('/{record}/edit'),
+        ];
+    }
+}
