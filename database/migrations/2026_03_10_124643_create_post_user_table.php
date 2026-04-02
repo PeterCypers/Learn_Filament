8a9c32c503285dbe566788ade0efb01eac7025ff
PeterCypers <peter-cypers@hotmail.com>
2026-04-02 11:52:50 +0200

    video #11 completed

--- database/migrations/2026_03_10_124643_create_post_user_table.php
+++ database/migrations/2026_03_10_124643_create_post_user_table.php
@@ -19,6 +19,7 @@ return new class extends Migration
             $table->foreignIdFor(User::class);
             $table->integer('order')->default(0);
             $table->timestamps();
+            // $table->softDeletes();
         });
     }
 
